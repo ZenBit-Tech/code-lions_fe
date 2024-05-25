72f11f83dc30c29e78768dc83208a5f14402c9c6
MargaritaSukhova <margarytasu@gmail.com>
2024-05-25 15:32:17 +0300

    chore: add .husky folder, github PR template, eslint rules, prettier rules update .gitignore

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules/**', 'vite-env.d.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: ['variable', 'function'],
+        format: ['camelCase', 'PascalCase'],
+      },
+    ],
+    'no-magic-numbers': [
+      'error',
+      {
+        ignore: [0, 1],
+        ignoreArrayIndexes: true,
+        enforceConst: true,
+        detectObjects: false,
+      },
+    ],
+    'no-constant-condition': 'error',
+    'no-nested-ternary': 'error',
+    'no-unused-vars': [
+      'error',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'none' },
+    ],
+    camelcase: ['error', { properties: 'never' }],
+    'spaced-comment': ['warn', 'always'],
+
+    'react/forbid-component-props': ['error', { forbid: ['style'] }],
+    'import/no-relative-parent-imports': 'error',
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
