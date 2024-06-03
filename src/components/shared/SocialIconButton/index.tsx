c67378f5aeaf53bb46b84e757afb5ef47c9bf466
MargaritaSukhova <margarytasu@gmail.com>
2024-06-03 23:14:38 +0300

    chore: updated styles for social-icon-button component

--- src/components/shared/SocialIconButton/index.tsx
+++ src/components/shared/SocialIconButton/index.tsx
@@ -3,12 +3,12 @@ import { styled } from '@mui/system';
 import theme from 'src/theme';
 
 const SocialIconButton = styled(IconButton)({
+  height: '44px',
   width: '100%',
   color: theme.palette.common.black,
   padding: '10px 34px',
   border: `1px solid ${theme.palette.border.secondary}`,
   borderRadius: '6px',
-  marginRight: '8px',
   cursor: 'pointer',
 });
 
