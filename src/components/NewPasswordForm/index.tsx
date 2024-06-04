f13517592718a681b18e6f2bda3f2e696c5c71f2
Oleksandra Kuchuhurna <sashyliakych@gmail.com>
2024-06-04 17:11:02 +0300

    feat: create new password screen

---
+++ src/components/NewPasswordForm/index.tsx
@@ -0,0 +1,140 @@
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { Box, Typography } from '@mui/material';
+import PasswordInput from 'src/components/shared/PasswordInput';
+import {
+  InputPaddingVariants,
+  InputStyleVariants,
+} from 'src/components/shared/StyledInput/types';
+import StyledButton from 'src/components/shared/StyledButton';
+import {
+  PaddingVariants,
+  StyleVariants,
+} from 'src/components/shared/StyledButton/types';
+import LabelText from 'src/components/shared/LabelText';
+import TitleInputWrapper from 'src/components/shared/TitleInputWrapper';
+import { styled } from '@mui/system';
+import { validations } from 'src/common/constants';
+import theme from 'src/theme';
+
+const FormWrapper = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  gap: '20px',
+  width: '100%',
+});
+
+interface IFormInput {
+  password: string;
+  repeatPassword: string;
+}
+
+function NewPasswordForm() {
+  const { t } = useTranslation();
+
+  const {
+    control,
+    watch,
+    handleSubmit,
+    formState: { isDirty, isValid, errors },
+  } = useForm<IFormInput>({
+    defaultValues: {
+      password: '',
+      repeatPassword: '',
+    },
+    mode: 'onChange',
+  });
+
+  const errorsLength: number = Object.keys(errors).length;
+  const password = watch('password');
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    return data;
+  };
+
+  return (
+    <FormWrapper onSubmit={handleSubmit(onSubmit)}>
+      <TitleInputWrapper>
+        <LabelText> {t('newPassword.passwordLabel')} </LabelText>
+        <Controller
+          name="password"
+          control={control}
+          rules={{
+            required: t('newPasswordErrors.enterCredentials'),
+            minLength: {
+              value: validations.PASSWORD_MIN_LENGTH,
+              message: t('newPasswordErrors.passwordLength'),
+            },
+          }}
+          render={({ field }) => (
+            <Box>
+              <PasswordInput
+                fullWidth
+                autoComplete="off"
+                placeholder={t('inputPlaceholder.userPassword')}
+                padding={InputPaddingVariants.MD}
+                stylevariant={InputStyleVariants.OUTLINED}
+                error={!!errors.password}
+                {...field}
+              />
+              {errors.password && (
+                <Typography mt={1} color={theme.palette.error.main}>
+                  {errors.password.message}
+                </Typography>
+              )}
+            </Box>
+          )}
+        />
+      </TitleInputWrapper>
+      <TitleInputWrapper>
+        <LabelText> {t('newPassword.repeatPasswordLabel')} </LabelText>
+        <Controller
+          name="repeatPassword"
+          control={control}
+          rules={{
+            required: t('newPasswordErrors.enterCredentials'),
+            minLength: {
+              value: validations.PASSWORD_MIN_LENGTH,
+              message: t('newPasswordErrors.passwordLength'),
+            },
+            validate: (value) =>
+              value === password || t('newPasswordErrors.passwordsNotMatch'),
+          }}
+          render={({ field }) => (
+            <Box>
+              <PasswordInput
+                fullWidth
+                autoComplete="off"
+                placeholder={t('inputPlaceholder.userPassword')}
+                padding={InputPaddingVariants.MD}
+                stylevariant={InputStyleVariants.OUTLINED}
+                error={!!errors.repeatPassword}
+                {...field}
+              />
+              {errors.repeatPassword && (
+                <Typography mt={1} color={theme.palette.error.main}>
+                  {errors.repeatPassword.message}
+                </Typography>
+              )}
+            </Box>
+          )}
+        />
+      </TitleInputWrapper>
+      <StyledButton
+        fullWidth
+        type="submit"
+        styles={StyleVariants.BLACK}
+        padding={PaddingVariants.LG}
+        disabled={!isDirty || !isValid || errorsLength > 0}
+      >
+        <Typography variant="button" color={theme.palette.common.white}>
+          {' '}
+          {t('newPassword.savePasswordButton')}{' '}
+        </Typography>
+      </StyledButton>
+    </FormWrapper>
+  );
+}
+
+export default NewPasswordForm;
