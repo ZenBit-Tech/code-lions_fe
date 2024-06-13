196480f6251cfe15d8e23da012434c0436a99c9a
zweroboy1 <fprognoz@gmail.com>
2024-06-14 02:13:21 +0300

    fix: remove inline styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from 'react-i18next';
 
+import { Box } from '@mui/system';
+
 function Header() {
   const { t } = useTranslation();
 
-  return <div style={{ height: '68px' }}>{t('header.home')}</div>;
+  return <Box>{t('header.home')}</Box>;
 }
 
 export default Header;
