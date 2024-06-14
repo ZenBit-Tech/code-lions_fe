1bb3acc75dc7b32357353f34ca68efb181d3fa14
MargaritaSukhova <margarytasu@gmail.com>
2024-06-14 16:36:45 +0300

    fix: updated size guide page with section title component

--- src/pages/SizesGuidePage/index.tsx
+++ src/pages/SizesGuidePage/index.tsx
@@ -5,8 +5,7 @@ import { Box } from '@mui/material';
 import Footer from 'src/components/Footer';
 import Header from 'src/components/Header';
 import InfoTable from 'src/components/shared/InfoTable';
-import Title from 'src/components/shared/Title';
-import { TitleWrapper } from 'src/pages/TermsOfUsePage/styles';
+import SectionTitle from 'src/components/shared/SectionTitle';
 
 import StyledHeader3 from './styles';
 import { clothesSizeData, shoeSizeData } from './tableData';
@@ -17,11 +16,7 @@ function SizesGuidePage() {
   return (
     <>
       <Header />
-      <TitleWrapper>
-        <Title variant="h5" component="h1">
-          {t('sizesGuide.title')}
-        </Title>
-      </TitleWrapper>
+      <SectionTitle title={t('sizesGuide.title')} greyBackground showBackLink />
       <Box component="section" sx={{ width: '100%', padding: '0 18px' }}>
         <Box component="div" sx={{ maxWidth: '920px', margin: '0 auto' }}>
           <StyledHeader3 component="h3">
