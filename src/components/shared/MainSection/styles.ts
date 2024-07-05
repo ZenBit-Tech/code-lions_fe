082c9874b5f7b8e5776330c62a3fc65cfabd468e
Oleksandra Kuchuhurna <sashyliakych@gmail.com>
2024-07-05 16:40:36 +0300

    fix: deleting not my components

--- src/components/shared/MainSection/styles.ts
+++
@@ -1,9 +0,0 @@
-import { Box, styled } from '@mui/system';
-
-const SectionStyled = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.grey[300],
-  padding: '30px 32px',
-  minHeight: 'calc(100% - 70px)',
-}));
-
-export default SectionStyled;
