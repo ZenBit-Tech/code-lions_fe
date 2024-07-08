cd8102932a51918557e2ab683852758b009f4072
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2024-07-08 13:25:06 +0300

    fix/removed slider from product card

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { Box, IconButton, Typography } from '@mui/material';
 
@@ -9,7 +9,6 @@ import BagIcon from 'src/assets/icons/profile/bag-duotone.svg';
 import BlackHeartIcon from 'src/assets/icons/profile/heart-black.svg';
 import RedHeartIcon from 'src/assets/icons/profile/heart-red.svg';
 import { urls } from 'src/common/constants';
-import ProductSliderModal from 'src/components/ProductSliderModal';
 import {
   useAddToCartMutation,
   useRemoveFromCartMutation,
@@ -30,13 +29,15 @@ interface IProductCardProps {
   item: IProduct;
 }
 
-const initialSlideIndex: number = 0;
+const duration: number = 7;
 
 function ProductCard({ item }: IProductCardProps) {
   const { images, name, vendor, price } = item;
 
   const userId = useSelector(selectUserId);
-  const [open, setOpen] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
   const [isInWishlist, setIsInWishlist] = useState<boolean>(false);
   const [isInCart, setIsInCart] = useState<boolean>(false);
 
@@ -73,31 +74,40 @@ function ProductCard({ item }: IProductCardProps) {
     }
   }, [cartData, item.id]);
 
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  const handleAddToWishlist = async () => {
+  const handleAddToWishlist = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.stopPropagation();
     if (userId) {
       await addToWishlist({ userId, productId: item.id }).unwrap();
       setIsInWishlist(true);
     }
   };
 
-  const handleRemoveFromWishlist = async () => {
+  const handleRemoveFromWishlist = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.stopPropagation();
     if (userId) {
       await removeFromWishlist({ userId, productId: item.id }).unwrap();
       setIsInWishlist(false);
     }
   };
 
-  const handleAddToCart = async () => {
+  const handleAddToCart = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.stopPropagation();
     if (userId) {
-      await addToCart({ userId, productId: item.id, duration: 7 }).unwrap();
+      await addToCart({ userId, productId: item.id, duration, price }).unwrap();
       setIsInCart(true);
     }
   };
 
-  const handleRemoveFromCart = async () => {
+  const handleRemoveFromCart = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.stopPropagation();
     if (userId) {
       await removeFromCart({ userId, productId: item.id }).unwrap();
       setIsInCart(false);
@@ -105,112 +115,104 @@ function ProductCard({ item }: IProductCardProps) {
   };
 
   return (
-    <>
-      <ProductSliderModal
-        open={open}
-        handleClose={handleClose}
-        images={images}
-        initialSlideIndex={initialSlideIndex}
-      />
-      <Box sx={style.card}>
-        <Box sx={style.cardWrapper}>
-          <Box sx={style.imgContainer}>
-            <Box sx={style.imgLink}>
-              <Box
-                component="img"
-                src={images[0]}
-                alt="product"
-                sx={style.imgWrapper}
-                onClick={handleOpen}
-              />
-            </Box>
+    <Box
+      sx={style.card}
+      onClick={() => navigate(`${urls.PRODUCT_FEED}/${item.id}`)}
+    >
+      <Box sx={style.cardWrapper}>
+        <Box sx={style.imgContainer}>
+          <Box sx={style.imgLink}>
+            <Box
+              component="img"
+              src={images[0]}
+              alt="product"
+              sx={style.imgWrapper}
+            />
           </Box>
-          {userId && (
-            <>
-              {isInWishlist ? (
-                <IconButton
-                  sx={style.heartIcon}
-                  onClick={handleRemoveFromWishlist}
-                >
-                  <RedHeartIcon />
-                </IconButton>
-              ) : (
-                <IconButton sx={style.heartIcon} onClick={handleAddToWishlist}>
-                  <BlackHeartIcon />
-                </IconButton>
-              )}
-            </>
-          )}
-          <Box sx={style.productInfoWrapper}>
-            <Box width="100%">
-              <Typography
-                variant="button"
-                component="h3"
-                sx={{
-                  lineHeight: '28px',
-                  overflow: 'hidden',
-                  display: '-webkit-box',
-                  WebkitBoxOrient: 'vertical',
-                  WebkitLineClamp: 1,
-                  textOverflow: 'ellipsis',
-                }}
+        </Box>
+        {userId && (
+          <>
+            {isInWishlist ? (
+              <IconButton
+                sx={style.heartIcon}
+                onClick={handleRemoveFromWishlist}
               >
-                <Link to={`${urls.PRODUCT_FEED}/${item.id}`}>{name}</Link>
-              </Typography>
-              <Box sx={style.productInfo}>
-                <Box>
-                  <Typography
-                    variant="h4"
+                <RedHeartIcon />
+              </IconButton>
+            ) : (
+              <IconButton sx={style.heartIcon} onClick={handleAddToWishlist}>
+                <BlackHeartIcon />
+              </IconButton>
+            )}
+          </>
+        )}
+        <Box sx={style.productInfoWrapper}>
+          <Box width="100%">
+            <Typography
+              variant="button"
+              component="h3"
+              sx={{
+                lineHeight: '28px',
+                overflow: 'hidden',
+                display: '-webkit-box',
+                WebkitBoxOrient: 'vertical',
+                WebkitLineClamp: 1,
+                textOverflow: 'ellipsis',
+              }}
+            >
+              <Link to={`${urls.PRODUCT_FEED}/${item.id}`}>{name}</Link>
+            </Typography>
+            <Box sx={style.productInfo}>
+              <Box>
+                <Typography
+                  variant="h4"
+                  sx={{
+                    color: theme.palette.text.disabled,
+                    lineHeight: '22px',
+                  }}
+                >
+                  ${price}
+                </Typography>
+                <Typography variant="subtitle2">
+                  <Link to={`${urls.VENDOR}/${vendor.id}`}>{vendor.name}</Link>
+                </Typography>
+              </Box>
+              <Box sx={style.bagIconWrapper}>
+                {isInCart ? (
+                  <IconButton
                     sx={{
-                      color: theme.palette.text.disabled,
-                      lineHeight: '22px',
+                      backgroundColor: theme.palette.common.black,
+                      opacity: 0.3,
+                      '.css-y47paa-MuiButtonBase-root-MuiIconButton-root:hover':
+                        {
+                          backgroundColor: theme.palette.common.black,
+                          opacity: 0.3,
+                        },
                     }}
+                    onClick={handleRemoveFromCart}
                   >
-                    ${price}
-                  </Typography>
-                  <Typography variant="subtitle2">
-                    <Link to={`${urls.VENDOR}/${vendor.id}`}>
-                      {vendor.name}
-                    </Link>
-                  </Typography>
-                </Box>
-                <Box sx={style.bagIconWrapper}>
-                  {isInCart ? (
-                    <IconButton
-                      sx={{
-                        backgroundColor: theme.palette.common.black,
-                        opacity: 0.3,
-                        '.css-y47paa-MuiButtonBase-root-MuiIconButton-root:hover':
-                          {
-                            backgroundColor: theme.palette.common.black,
-                            opacity: 0.3,
-                          },
-                      }}
-                      onClick={handleRemoveFromCart}
-                    >
-                      <BagIcon />
-                    </IconButton>
-                  ) : (
-                    <IconButton
-                      sx={{
-                        backgroundColor: theme.palette.common.black,
-                        '.css-y47paa-MuiButtonBase-root-MuiIconButton-root:hover':
-                          {
-                            backgroundColor: theme.palette.common.black,
-                          },
-                      }}
-                      onClick={handleAddToCart}
-                    >
-                      <BagIcon />
-                    </IconButton>
-                  )}
-                </Box>
+                    <BagIcon />
+                  </IconButton>
+                ) : (
+                  <IconButton
+                    sx={{
+                      backgroundColor: theme.palette.common.black,
+                      '.css-y47paa-MuiButtonBase-root-MuiIconButton-root:hover':
+                        {
+                          backgroundColor: theme.palette.common.black,
+                        },
+                    }}
+                    onClick={handleAddToCart}
+                  >
+                    <BagIcon />
+                  </IconButton>
+                )}
               </Box>
             </Box>
           </Box>
         </Box>
       </Box>
-    </>
+    </Box>
   );
 }
 
