31ae843f3ab414a3baa49fc1d98ee78d444dd9dc
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2024-07-10 22:30:17 +0300

    feat/added unit tests

--- src/redux/product/productService.test.tsx
+++ src/redux/product/productService.test.tsx
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 
 import store from 'src/redux/store';
 import allProducts from 'src/test/mocks/allproducts';
+import singleProduct from 'src/test/mocks/singleProduct';
 
 import { productApi } from './productService';
 
 describe('productApi', () => {
-  const { useGetProductsQuery } = productApi;
+  const { useGetProductsQuery, useGetProductByIdQuery } = productApi;
 
   it('fetches products successfully', async () => {
     const { result } = renderHook(() => useGetProductsQuery(), {
@@ -16,5 +17,14 @@ describe('productApi', () => {
     });
 
     await waitFor(() => expect(result.current.data).toEqual(allProducts));
+  });
+
+  it('fetches a product by ID successfully', async () => {
+    const productId = singleProduct.id;
+    const { result } = renderHook(() => useGetProductByIdQuery({ productId }), {
+      wrapper: ({ children }) => <Provider store={store}>{children}</Provider>,
+    });
+
+    await waitFor(() => expect(result.current.data).toEqual(singleProduct));
   });
 });
