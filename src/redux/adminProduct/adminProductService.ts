e25da03f951fc8420d4dead19a4a7e45a6c8a838
MargaritaSukhova <margarytasu@gmail.com>
2024-07-18 14:27:28 +0300

    fix: updated admin product service with baseQueryWithReauth

--- src/redux/adminProduct/adminProductService.ts
+++ src/redux/adminProduct/adminProductService.ts
@@ -1,74 +1,13 @@
-import {
-  BaseQueryApi,
-  FetchArgs,
-  createApi,
-  fetchBaseQuery,
-} from '@reduxjs/toolkit/query/react';
-import {
-  apiUrl,
-  HttpMethods,
-  RTKUrls,
-  httpStatusCodes,
-} from 'src/common/constants.ts';
-import { RootState } from 'src/redux/store';
-import { setTokens, logout } from 'src/redux/user/userSlice';
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { HttpMethods, RTKUrls } from 'src/common/constants.ts';
 
-import { IRefreshTokenResponse } from '../user/types';
+import { baseQueryWithReauth } from '../user/userService';
 
 import {
   IAdminProductsResponse,
   IAdminProductsRequest,
   IAdminEditProductRequest,
 } from './types';
-
-const baseQuery = fetchBaseQuery({
-  baseUrl: apiUrl,
-  prepareHeaders: (headers, { getState }) => {
-    const { accessToken } = (getState() as RootState).user;
-
-    if (accessToken) {
-      headers.set('Authorization', `Bearer ${accessToken}`);
-    }
-
-    return headers;
-  },
-});
-
-export const baseQueryWithReauth = async (
-  args: string | FetchArgs,
-  api: BaseQueryApi,
-  extraOptions: object
-) => {
-  let result = await baseQuery(args, api, extraOptions);
-
-  if (result.error && result.error.status === httpStatusCodes.UNAUTHORIZED) {
-    const state = api.getState() as RootState;
-    const { refreshToken } = state.user;
-
-    if (refreshToken) {
-      const refreshResult = await baseQuery(
-        {
-          url: RTKUrls.REFRESH_TOKEN,
-          method: 'POST',
-          body: { refreshToken },
-        },
-        api,
-        extraOptions
-      );
-
-      if (refreshResult.data) {
-        api.dispatch(setTokens(refreshResult.data as IRefreshTokenResponse));
-        result = await baseQuery(args, api, extraOptions);
-      } else {
-        api.dispatch(logout());
-      }
-    } else {
-      api.dispatch(logout());
-    }
-  }
-
-  return result;
-};
 
 export const adminProductsApi = createApi({
   reducerPath: 'adminProductsApi',
