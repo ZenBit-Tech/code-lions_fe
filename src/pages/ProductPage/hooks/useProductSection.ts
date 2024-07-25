baa7cc9096d6e2af4201bd7b36d767ddf485c6fa
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2024-07-25 20:14:29 +0300

    feat/added dynamic rent duration options for high rated buyers

--- src/pages/ProductPage/hooks/useProductSection.ts
+++ src/pages/ProductPage/hooks/useProductSection.ts
@@ -1,9 +1,9 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
 
 import { SerializedError } from '@reduxjs/toolkit';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
+import { skipToken } from '@reduxjs/toolkit/query/react';
 import { getErrorMessage } from 'src/common/hooks/useErrorHandling';
 import useToast from 'src/components/shared/toasts/components/ToastProvider/ToastProviderHooks';
 import {
@@ -13,34 +13,53 @@ import {
 import { ICartItem } from 'src/redux/cart/types';
 import { useAppSelector } from 'src/redux/hooks';
 import { IProduct } from 'src/redux/product/types';
-import { selectHideRentalRules, selectUserId } from 'src/redux/user/userSlice';
+import { useGetUserReviewsQuery } from 'src/redux/user/userService';
+import {
+  selectHideRentalRules,
+  selectUser,
+  selectUserId,
+} from 'src/redux/user/userSlice';
 
-const durations = [
-  { duration: 7, price: 0 },
-  { duration: 14, price: 0 },
-];
-
-const weeksCount = 2;
+const weeksCount: number = 2;
+const threeFiveStarsRatings: number = 3;
+const averageRatingFourPointNine: number = 4.9;
+const fiveStarsRating: number = 5;
 
 const useProductSection = (product: IProduct) => {
   const { t } = useTranslation();
   const { showToast } = useToast();
 
-  const userId = useSelector(selectUserId);
-  const willHideRentalRules = useSelector(selectHideRentalRules);
+  const userId = useAppSelector(selectUserId);
+  const willHideRentalRules = useAppSelector(selectHideRentalRules);
+  const user = useAppSelector(selectUser);
+  const cartData = useAppSelector((state) => state.cart);
 
   const [selectedSize] = useState<string>(product.size);
-  const [value, setValue] = useState<string>(durations[0]?.duration.toString());
+  const [value, setValue] = useState<string>('');
   const [showModal, setShowModal] = useState<boolean>(false);
-
-  durations[0].price = product.price;
-  durations[1].price = product.price * weeksCount;
-
-  const cartData = useAppSelector((state) => state.cart);
 
   const [addToCart, { isLoading: isAddingToCart }] = useAddToCartMutation();
   const [removeFromCart, { isLoading: isRemovingFromCart }] =
     useRemoveFromCartMutation();
+  const { data: reviewsData } = useGetUserReviewsQuery(userId || skipToken);
+
+  const hasThreeFiveStarReviews = reviewsData
+    ? reviewsData.filter((review) => review.rating === fiveStarsRating)
+        .length >= threeFiveStarsRatings
+    : false;
+  const isHighRated = user ? user.rating >= averageRatingFourPointNine : false;
+  const isEligibleForExtendedPrivileges =
+    hasThreeFiveStarReviews && isHighRated;
+
+  const durations = useMemo(() => {
+    const baseDurations = [{ duration: 7, price: product.price }];
+
+    if (isEligibleForExtendedPrivileges) {
+      baseDurations.push({ duration: 14, price: product.price * weeksCount });
+    }
+
+    return baseDurations;
+  }, [isEligibleForExtendedPrivileges, product.price]);
 
   const isProductInCart = cartData?.some(
     (item: ICartItem) => item.productId === product.id
@@ -60,6 +79,8 @@ const useProductSection = (product: IProduct) => {
     );
 
     if (!selectedDuration) {
+      showToast('error', t('product.durationNotSelected'));
+
       return false;
     }
 
